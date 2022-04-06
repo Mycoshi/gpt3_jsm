0b2d5cd342c663c8f94f0107b61a3b53e3c035bc
Mycoshi <67665537+Mycoshi@users.noreply.github.com>
2022-04-06 12:07:50 -0700

    updated for desktop
    added whatgpt3 margins and features css

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ const whatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+          <Feature title="What is GPT-3" text="Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.." />
         </div>
         <div className="gpt3__whatgpt3-heading">
           <h1 className="gradient__text">The Possibilities are beyond your imagination</h1>
           <p>Explore the library</p>
           </div>
           <div className="gpt3__whatgpt3-container">
-          <Feature title="Chatbots" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
-          <Feature title="Knowledge base" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
-          <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
+          <Feature title="Chatbots" text="Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring." />
+          <Feature title="Knowledge base" text="Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line." />
+          <Feature title="Education" text="Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions." />
         </div>
       </div>
   )
